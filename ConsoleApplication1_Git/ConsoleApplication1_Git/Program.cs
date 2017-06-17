d5afe7d9903dee39f06ff215e3eab203cd94f171
chashmeshab <chashmeshab@C6126>
2017-06-17 10:52:55 +0430

    just add Console.WriteLine("Press any key to countinu ...");

---
+++ ConsoleApplication1_Git/ConsoleApplication1_Git/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApplication1_Git
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command;
+            Console.Write(Directory.GetCurrentDirectory() + ">");
+            command = Console.ReadLine().ToLower();
+            if (command == "hostname")
+            {
+                 Console.WriteLine(Environment.OSVersion);
+                Console.ReadKey();
+            }
+
+
+
+        }
+    }
+}
